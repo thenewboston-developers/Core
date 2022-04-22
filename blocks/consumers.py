e39ac0806f91a71dc4c056f1c8099f6a5add42cf
Bucky Roberts <buckyroberts@gmail.com>
2022-04-22 14:57:54 -0400

    Block sending working (#7)

--- blocks/consumers.py
+++ blocks/consumers.py
@@ -1,20 +1,35 @@
-import json
+import re
 
-from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import JsonWebsocketConsumer
 
 
-class BlockConsumer(WebsocketConsumer):
+class BlockConsumer(JsonWebsocketConsumer):
 
     def connect(self):
+        """Accepts an incoming socket"""
+        url_route = self.scope.get('url_route')
+
+        if url_route:
+            account_number = url_route['kwargs']['account_number']
+        else:
+            # We need this for testing, as url_route is not available during tests
+            account_number = re.match(
+                '^ws/blocks/(?P<account_number>[a-f0-9]{64})',
+                self.scope['path']
+            ).group(1)
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.group_name(account_number),
+            self.channel_name
+        )
         self.accept()
 
-    def disconnect(self, close_code):
-        pass
+    @staticmethod
+    def group_name(account_number):
+        """Name of group where messages will be broadcast"""
+        return 'blocks_%s' % account_number
 
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+    def send_block(self, event):
+        """Send block to group"""
+        self.send_json(event['message'])
