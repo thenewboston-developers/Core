fdf4d84d7f44ff70e0b7366c7922cdaf8560f912
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2022-04-26 19:36:32 +0300

    BlockViewSet refactoring

--- core/blocks/views/block.py
+++ core/blocks/views/block.py
@@ -1,14 +1,12 @@
 import channels.layers
 from asgiref.sync import async_to_sync
 from rest_framework.mixins import CreateModelMixin, ListModelMixin
-from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED
 from rest_framework.viewsets import GenericViewSet
 
 from core.blocks.consumers import BlockConsumer
 
 from ..models.block import Block
-from ..serializers.block import BlockSerializer, BlockSerializerCreate
+from ..serializers.block import BlockSerializer
 
 
 def send(block_data):
@@ -22,19 +20,8 @@ class BlockViewSet(CreateModelMixin, ListModelMixin, GenericViewSet):
     filterset_fields = ('amount', 'recipient', 'sender')
     queryset = Block.objects.order_by('id').all()
     serializer_class = BlockSerializer
-    serializer_create_class = BlockSerializerCreate
 
-    def create(self, request, *args, **kwargs):
-        # TODO(dmu) MEDIUM: Use PerActionSerializerMixin instead of overriding create()
-        serializer = self.serializer_create_class(
-            data=request.data,
-            context={'request': request},
-        )
-        serializer.is_valid(raise_exception=True)
-        block = serializer.save()
-        # TODO(dmu) MEDIUM: Consider not serializing the block again
-        block_data = self.get_serializer(block).data
-
-        send(block_data)
-
-        return Response(block_data, status=HTTP_201_CREATED)
+    def perform_create(self, serializer):
+        rv = super().perform_create(serializer)
+        send(serializer.data)
+        return rv
