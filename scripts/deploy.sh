8ebd94237f9b888a01ccc18a25526c0aaf34d602
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2022-04-28 12:40:10 +0300

    HTTPS support fixup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -32,7 +32,8 @@ grep -q -o CORESETTING_SECRET_KEY .env 2> /dev/null || echo "CORESETTING_SECRET_
 
 docker compose pull  # Ensure latest image is downloaded locally (even if tag did not change)
 
-docker compose run -it --rm certbot certificates | grep -q 'No certificates found' && (echo 'Installing certificates..'; docker compose run -it --rm certbot certonly --non-interactive --domain thenewboston.network --standalone)
+docker compose down
+docker compose run -it --rm certbot certificates | grep -q 'No certificates found' && (echo 'Installing certificates..'; docker compose run -it --rm certbot certonly --agree-tos --email dmugtasimov@gmail.com --non-interactive --domain thenewboston.network --standalone)
 
 echo 'Starting the Core API...'
 docker compose up -d --force-recreate
