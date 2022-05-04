a0b1d5f702b898ede8d7c5222e5f96eba2e6663f
bucky <buckyroberts@gmail.com>
2022-05-04 18:00:52 -0400

    Change middleware ordering

--- core/config/settings/base.py
+++ core/config/settings/base.py
@@ -27,9 +27,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
