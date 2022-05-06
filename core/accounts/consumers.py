be7c5c90277149a6192c947cfedd9a0cb9ad894d
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2022-05-06 12:11:15 +0300

    WS message typing

--- core/accounts/consumers.py
+++ core/accounts/consumers.py
@@ -1,5 +1,13 @@
+from enum import Enum
+
+import channels.layers
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import JsonWebsocketConsumer
+
+
+class MessageType(Enum):
+    CREATE_BLOCK = 'create.block'
+    UPDATE_ACCOUNT = 'update.account'
 
 
 class AccountConsumer(JsonWebsocketConsumer):
@@ -17,6 +25,19 @@ class AccountConsumer(JsonWebsocketConsumer):
         """Name of group where messages will be broadcast"""
         return f'blocks_{account_number}'
 
-    def send_block(self, event):
+    def create_block(self, event):
         """Send block to group"""
-        self.send_json(event['message'])
+        self.send_json(event)
+
+    def update_account(self, event):
+        """Send account to group"""
+        self.send_json(event)
+
+
+assert all(hasattr(AccountConsumer, item.value.replace('.', '_')) for item in MessageType)
+
+
+def send(message_type: MessageType, recipient: str, message: dict):
+    channel_layer = channels.layers.get_channel_layer()
+    payload = {'type': message_type.value, 'message': message}
+    async_to_sync(channel_layer.group_send)(AccountConsumer.group_name(recipient), payload)
