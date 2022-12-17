7c885004772009025f2f19b9cd32eac721b12ed6
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2022-12-17 19:55:41 +0600

    Migrate mtime monitoring

--- scripts/dockerized-nginx-run.sh
+++ scripts/dockerized-nginx-run.sh
@@ -2,43 +2,51 @@
 
 CERTIFICATE_FILE=/etc/letsencrypt/live/main/fullchain.pem
 
-set -em
+set -e
+set -m
 
-_quit() {
-  echo 'Gracefully closing nginx...'
+_wait_nginx() {
+  echo 'Bringing nginx to foreground and waiting it to exit...'
+  fg %1 || true  # bring nginx to foreground and fail silently if it already exited
+  echo 'nginx has existed'
+  # after exiting this script the container should restart and therefore updated certificate reread from storage
+}
+
+_stop_nginx() {
+  echo 'Gracefully stopping nginx...'
   # TODO(dmu) LOW: Will nginx still close WebSocket connections on SIGQUIT signal?
   kill -SIGQUIT "$NGINX_PID" || true  # fail silently if process not found
-  echo 'Signal is sent to nginx'
+  echo 'Gracefully stop signal is sent to nginx'
 }
 
 _handle_sigquit() {
-  echo 'Forwarding caught SIGQUIT signal to nginx and killing inotifywait'
-  quit
-  kill "$INOTIFYWAIT_PID" || true  # fail silently if process not found
+  echo 'Forwarding caught SIGQUIT signal to nginx'
+  _stop_nginx
+  _wait_nginx
+  exit
 }
 
-echo 'Running nginx in background, so we can run inotifywait later...'
+echo 'Running nginx in background, so we can watch mtime change later'
 nginx -g 'daemon off;' &
 NGINX_PID=$!  # save nginx pid
 echo 'nginx is running in background'
 
-trap _handle_sigquit SIGQUIT  # forward SIGQUIT to nginx and also kill inotifywait
+trap _handle_sigquit SIGQUIT
 
 echo "Waiting certificate file ($CERTIFICATE_FILE) to appear..."
-# Otherwise inotifywait will exit. This is important on installation from scratch
-until [ -f $CERTIFICATE_FILE ]; do
+until [ -f "$CERTIFICATE_FILE" ]; do
   sleep 60
 done
-echo "$CERTIFICATE_FILE has to appeared"
-echo 'Running inotifywait...'
-inotifywait -e modify $CERTIFICATE_FILE &  # in background to be able to grab its pid to kill later
-INOTIFYWAIT_PID=$!
-echo 'inotifywait is running'
-fg %%  # bring last job (inotifywait) to foreground, so we exit only once $CERTIFICATE_FILE is modified
+echo "$CERTIFICATE_FILE has appeared"
 
-_quit
+# We are not using inotifywait because it does not work well across shared docker volumes
+ORIGINAL_MTIME=$(stat -c %Y $CERTIFICATE_FILE)
+echo "$CERTIFICATE_FILE mtime: $ORIGINAL_MTIME"
+echo "Waiting for $CERTIFICATE_FILE mtime change..."
+while [ "$(stat -c %Y $CERTIFICATE_FILE)" == "$ORIGINAL_MTIME" ]; do
+  sleep 60
+done
+echo "$(date): mtime has changed"
 
-echo 'Bringing nginx to foreground and waiting it to exit...'
-fg %1 || true  # bring nginx to foreground and fail silently if it already exited
-echo 'nginx has existed'
-# after exiting this script the container should restart and therefore updated certificate reread from storage
+_stop_nginx
+_wait_nginx
