f1976213c9e72ba755286ab0e9c8d79c3be148d6
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2023-01-31 17:04:06 +0600

    CONN_MAX_AGE = 0

--- core/project/settings/base.py
+++ core/project/settings/base.py
@@ -67,7 +67,13 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
         'ATOMIC_REQUESTS': True,
-        'CONN_MAX_AGE': 600,
+        # TODO(dmu) MEDIUM: Unfortunately Daphne / ASGI / Django Channels do not properly reuse database connections
+        #                   and therefore we are getting resource (connection) leak that leads to the following:
+        #                   django.db.utils.OperationalError: FATAL:  sorry, too many clients already
+        #                   `'CONN_MAX_AGE': 0` is used as workaround. In case it notably affects performance
+        #                   implement a solution that either closes database connections on WebSocket client
+        #                   disconnect and implement connection pooling outside Django (BgBouncer or similar)
+        'CONN_MAX_AGE': 0,
     }
 }
 
